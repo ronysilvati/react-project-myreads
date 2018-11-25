ac2b0a692df93dbf9fe9f881ef65a08d280cb07b
Rony <ronysilvati@live.com>
2018-11-25 14:19:16 -0300

    feat: Added component of route
    The component of route allow the user to render conditionaly
    the page of "list" and page of "search".

--- src/App.js
+++ src/App.js
@@ -3,16 +3,10 @@ import React from 'react';
 import './App.css';
 import Bookshelf from './Bookshelf';
 import * as BooksAPI from './BooksAPI';
+import {Route, Link, BrowserRouter as Router} from 'react-router-dom';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     booksCurrentlyReading: [],
     booksWantToRead: [],
     booksRead: []
@@ -175,13 +169,14 @@ class BooksApp extends React.Component {
     let {booksCurrentlyReading,booksWantToRead,booksRead} = this.state;
 
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
+      <Router>
+        <div className="app">
+            <Route exact path="/search" render={()  => (
+              <div className="search-books">
+                <div className="search-books-bar">
+                  <button className="close-search">Close</button>
+                  <div className="search-books-input-wrapper">
+                    {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -189,32 +184,37 @@ class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                    <input type="text" placeholder="Search by title or author"/>
 
+                  </div>
+                </div>
+                <div className="search-books-results">
+                  <ol className="books-grid"></ol>
+                </div>
               </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>My Reads Radar</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Bookshelf title="Currently Reading" books={booksCurrentlyReading} movimentBook={this.movimentBookOnBookshelf.bind(this)}/>
-                <Bookshelf title="Want to Read" books={booksWantToRead} movimentBook={this.movimentBookOnBookshelf.bind(this)}/>
-                <Bookshelf title="Read" books={booksRead} movimentBook={this.movimentBookOnBookshelf.bind(this)}/>
+            )} />
+
+            <Route exact path="/" render={({history})  => (
+              <div className="list-books">
+                <div className="list-books-title">
+                  <h1>My Reads Radar</h1>
+                </div>
+                <div className="list-books-content">
+                  <div>
+                    <Bookshelf title="Currently Reading" books={booksCurrentlyReading} movimentBook={this.movimentBookOnBookshelf.bind(this)}/>
+                    <Bookshelf title="Want to Read" books={booksWantToRead} movimentBook={this.movimentBookOnBookshelf.bind(this)}/>
+                    <Bookshelf title="Read" books={booksRead} movimentBook={this.movimentBookOnBookshelf.bind(this)}/>
+                  </div>
+                </div>
+                <div className="open-search">
+                  <Link to="/search">
+                    <button>Add a book</button>
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-            </div>
-          </div>
-        )}
-      </div>
+            )} />
+        </div>
+      </Router>
     )
   }
 }
